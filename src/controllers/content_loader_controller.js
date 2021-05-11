d4915743de2eebb94f73417cd8be0d40c7b1b6b9
sachinkreeti <sgupta@kreeti.com>
2021-05-11 15:59:13 +0530

    feat: add timer to fetch messages at regular intervals

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -1,10 +1,31 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static values = { url: String };
+  static values = { url: String, refreshInterval: Number };
 
   connect() {
     this.load();
+
+    if (this.refreshIntervalValue) {
+      this.startRefreshing();
+    }
+  }
+
+  disconnect() {
+    this.stopRefreshing();
+  }
+
+  startRefreshing() {
+    this.refreshTimer = setInterval(
+      () => this.load(),
+      this.refreshIntervalValue
+    );
+  }
+
+  stopRefreshing() {
+    if (this.refreshTimer) {
+      clearInterval(this.refreshTimer);
+    }
   }
 
   load() {
